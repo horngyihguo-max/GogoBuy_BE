44404fb971c956d3118b651c0aef1bebc306b838
user\Mickey <mickey11566@gmail.com>
2026-01-12 11:41:43 +0800

    fix: ResMessage conflict

--- src/main/java/com/example/demo/constants/ResMessage.java
+++ src/main/java/com/example/demo/constants/ResMessage.java
@@ -12,6 +12,18 @@ public enum ResMessage {
 	STORE_EXISTS(400,"店家(電話)已存在"),
 	STORE_NOT_FOUND(404,"找不到該店家"),
 	INPUT_IS_EMPTY(400,"請輸入搜尋關鍵字"),
+	OUT_OF_TIMES_REMAINING(400, "超過許願次數"),
+	WISH_ID_NOT_FOUND(400, "願望不存在"),
+    SAME_PASSWORD_ERROR(400, "新密碼與舊密碼相同"),
+    EMAIL_ERROR(500, "信箱發送失敗"),
+    OTP_EMPTY(400, "請先取得驗證碼"),    
+    OTP_EXPIRED(400, "驗證碼已過期"),    
+    OTP_ERROR(400, "驗證碼錯誤"),    
+    EMAIL_EXITS(400, "該信箱已被使用"),
+    EMAIL_SUCCESS(200, "認證信已寄出"),
+    PHONE_ERROR(400, "請輸入手機號碼"),
+	WISH_USER_CAN_NOT_FOLLOW(400, "許願者不可跟願"),
+	WISH_DELETE_ERROR(400, "願望刪除失敗"),
 	USER_NOT_FOUND(404, "找不到此用戶");
 	
 	
