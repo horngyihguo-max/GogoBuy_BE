3a673a75c16224509db706d6a8bc227b8cd3f9ab
himiki <0816shark@gmail.com>
2026-01-12 11:49:03 +0800

    no message

--- src/main/java/com/example/demo/service/CouponService.java
+++ src/main/java/com/example/demo/service/CouponService.java
@@ -1,5 +1,16 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.CouponsDao;
+
+@Service
 public class CouponService {
+	
+	@Autowired
+	private CouponsDao couponsDao;
+	
+	
 
 }
